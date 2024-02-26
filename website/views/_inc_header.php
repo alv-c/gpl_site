e4e4d31779a36bdc85b20fd8919e946a0eeb3bcf
Álvaro Carvalho <alvaro.carvalhohdr@gmail.com>
2024-02-26 17:02:37 -0300

    interna-noticia&navbar-superior

--- website/views/_inc_header.php
+++ website/views/_inc_header.php
@@ -1,4 +1,4 @@
-<nav>
+<!-- <nav>
     <div class="container">
         <a href="#">
             <img src="/website/img/global/logo.png" alt="GPL Incorporadora">
@@ -46,5 +46,77 @@
                 </a>
             </li>
         </ul>
+    </div>
+</nav> -->
+
+
+<nav class="navbar navbar-expand-xl navbar-light bg-white">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="/website/img/global/logo.png" alt="GPL Incorporadora">
+        </a>
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+
+        <button class="menu" onclick="this.classList.toggle('opened');this.setAttribute('aria-expanded', this.classList.contains('opened'))" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <svg width="50" height="50" viewBox="0 0 100 100">
+                <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                <path class="line line2" d="M 20,50 H 80" />
+                <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+            </svg>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item d-xl-none">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Institucional</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Empreendimentos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">GPL Acontece</a>
+                </li>
+                <li class="nav-item d-xl-none">
+                    <a class="nav-link" href="#">Trabalhe conosco</a>
+                </li>
+            </ul>
+            <div class="contain-btn-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <div class="ico">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="25" viewBox="0 0 26 25" fill="none">
+                                <mask id="mask0_46_1426" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="26" height="25">
+                                    <rect x="0.954102" y="0.414291" width="24.1714" height="24.1714" fill="#D9D9D9" />
+                                </mask>
+                                <g>
+                                    <path d="M8.0048 14.3593C8.52128 14.3593 8.96029 14.1786 9.32184 13.8171C9.68337 13.4555 9.86414 13.0165 9.86414 12.5C9.86414 11.9835 9.68337 11.5445 9.32184 11.183C8.96029 10.8214 8.52128 10.6407 8.0048 10.6407C7.48832 10.6407 7.04931 10.8214 6.68776 11.183C6.32623 11.5445 6.14546 11.9835 6.14546 12.5C6.14546 13.0165 6.32623 13.4555 6.68776 13.8171C7.04931 14.1786 7.48832 14.3593 8.0048 14.3593ZM8.0048 17.2646C6.66194 17.2646 5.53213 16.8062 4.61536 15.8894C3.69861 14.9727 3.24023 13.8429 3.24023 12.5C3.24023 11.1572 3.69861 10.0273 4.61536 9.11057C5.53213 8.19382 6.66194 7.73544 8.0048 7.73544C9.14494 7.73544 10.1324 8.05696 10.9672 8.69998C11.8019 9.343 12.3368 10.1061 12.5718 10.9893H21.6845L23.1894 12.4942L20.6909 15.1476L18.8509 13.7589L17.011 15.2116L15.4421 14.0107H12.5718C12.3329 14.9094 11.7932 15.6764 10.9526 16.3117C10.1121 16.9469 9.12944 17.2646 8.0048 17.2646Z" />
+                                </g>
+                            </svg>
+                        </div>
+                        <span>Portal do Cliente</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <div class="ico">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
+                                <mask id="mask0_46_1434" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="26">
+                                    <rect x="0.6875" y="0.914291" width="24.1714" height="24.1714" fill="#D9D9D9" />
+                                </mask>
+                                <g>
+                                    <path d="M19.7532 21.0571C18.0553 21.0571 16.3147 20.632 14.5316 19.7817C12.7484 18.9315 11.0886 17.7381 9.55205 16.2016C8.02841 14.665 6.84146 13.0084 5.99122 11.2317C5.14095 9.45498 4.71582 7.71766 4.71582 6.01972C4.71582 5.71757 4.81653 5.46256 5.01796 5.25467C5.21939 5.04679 5.47118 4.94286 5.77332 4.94286H8.26406C8.5378 4.94286 8.77668 5.02904 8.98069 5.20141C9.18471 5.37379 9.32286 5.59491 9.39516 5.86477L9.89485 8.26643C9.94135 8.54791 9.93295 8.79388 9.86967 9.00436C9.80641 9.21482 9.69472 9.38719 9.53462 9.52148L7.3247 11.5822C7.7379 12.3324 8.19467 13.0294 8.69502 13.673C9.19535 14.3167 9.72893 14.9265 10.2958 15.5024C10.8807 16.0873 11.5102 16.6312 12.1842 17.1341C12.8582 17.6371 13.598 18.1119 14.4038 18.5586L16.5575 16.3662C16.7215 16.1893 16.9058 16.0728 17.1105 16.0166C17.3151 15.9604 17.5414 15.9491 17.7893 15.9827L19.9082 16.4165C20.1819 16.4837 20.404 16.6215 20.5744 16.83C20.7449 17.0386 20.8301 17.2778 20.8301 17.5476V19.9996C20.8301 20.3018 20.7262 20.5536 20.5183 20.755C20.3104 20.9564 20.0554 21.0571 19.7532 21.0571Z" />
+                                </g>
+                            </svg>
+                        </div>
+                        <span>Central de atendimento</span>
+                    </a>
+                </li>
+            </div>
+        </div>
     </div>
 </nav>
---
+++ website/views/noticia.php
@@ -0,0 +1,84 @@
+	<?php
+	$pagina = "noticia";
+	include "_inc_headers.php";
+	?>
+	<title>Notícia</title>
+	</head>
+
+	<body id="<?php echo $pagina; ?>">
+	
+		<?php
+		include "_inc_header.php";
+		?>
+
+		<section class="noticia-topo">
+			<div class="container">
+				<span class="data-noticia">20/12/2023</span>
+				<span class="titulo-noticia">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem ispum.
+				</span>
+				<img src="/website/img/noticia/noticia-banner.png" alt="Noticia GPL" class="banner-noticia">
+			</div>
+		</section>
+		<section class="noticia-desc">
+			<div class="container">
+				<article>
+					<span>
+						rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+						with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</span>
+					<span>
+						rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+						with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</span>
+					<span>
+						rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+						with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</span>
+					<span>
+						rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+						It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+						It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+						with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</span>
+				</article>
+			</div>
+		</section>
+		<section class="noticia-imgs-grid">
+			<div class="container">
+				<div class="grid-imgs">
+					<img src="website/img/noticia/img-noticia-1.png">
+					<img src="website/img/noticia/img-noticia-2.png">
+					<img src="website/img/noticia/img-noticia-3.png">
+				</div>
+				<div class="contain-btn-voltar">
+					<a href="#" class="btn-voltar">
+						<div class="ico">
+							<svg xmlns="http://www.w3.org/2000/svg" width="9" height="9" viewBox="0 0 9 9" fill="none">
+								<mask id="mask0_350_1495" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="9" height="9">
+									<rect width="9" height="9" fill="#D9D9D9" />
+								</mask>
+								<g>
+									<path d="M3.75 8.25L0 4.5L3.75 0.75L4.41563 1.41562L1.33125 4.5L4.41563 7.58438L3.75 8.25Z" fill="#1C1B1F" />
+								</g>
+							</svg>
+						</div>
+						<span>Voltar</span>
+					</a>
+				</div>
+			</div>
+		</section>
+		<?php include "_inc_footer.php"; ?>
+	</body>
+
+	</html>
